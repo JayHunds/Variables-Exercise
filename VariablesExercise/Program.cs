a9f20376f5be70d175b5cc02502ba4114748dba5
Jayson Hundsrucker <hundsruckerjayson@gmail.com>
2024-02-15 10:42:15 -0800

    Fixed variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -6,10 +6,10 @@
         {
             string dogName = "Ralph";
             int dogAge = 10;
-            char = "1";
-            bool = true
-            double = 3.14;
-            decimal = 1.345m;
+            char myChar = "1";
+            bool myBool = true
+            double myDouble = 3.14;
+            decimal myDecimal = 1.345m;
 
             Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old");
 
